5fd008227275f1c57181428609659990ea626e92
jooooooing <leegun1018@naver.com>
2022-04-18 20:40:23 +0900

    Update P02.java

--- JavaAdvanced/Lecture03/P02.java
+++ JavaAdvanced/Lecture03/P02.java
@@ -7,7 +7,7 @@ public class P02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		int k31_iWeekDay = 6;
+		int k31_iWeekDay = 6;
 		int [] k31_iEnd = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		int k31_startDay =0;
 		int k31_endDay =0;
@@ -16,38 +16,23 @@ public class P02 {
 		Calendar end = Calendar.getInstance();
 		
 		for (int k31_iMon = 0; k31_iMon < 12; k31_iMon++) {
-//			start.set(2022, k31_iMon+1, 1);
-//			end.set(2022, k31_iMon+2, 1);
-//			end.add(Calendar.DATE, -1);
-//						
-//			k31_startDay = start.get(Calendar.DAY_OF_WEEK);
-//			k31_endDay = end.get(Calendar.DATE);
-//			
-//			System.out.println(k31_iMon+1 + "start" + k31_startDay);
-//			System.out.println(k31_iMon+2 + "end" + k31_endDay);
 			
 			System.out.printf("\n\n\t\t %d월\n", k31_iMon+1);
-			System.out.println("=======================");
-			System.out.println("일 월 화 수 목 금 토");
+			System.out.println("=============================");
+			System.out.printf("%3s%3s%3s%3s%3s%3s%3s\n","일","월","화","수","목","금","토");
 			
-			for (int i = 1; i <k31_startDay; i ++) {
-				System.out.print("v");
-			}
+			for (int i = 1; i <= k31_iWeekDay; i ++) {
+				 System.out.printf("%4s"," ");
+			}			
 			
-			int k31_iWeekDay = k31_startDay - 1;
 			for (int i = 1; i <= k31_iEnd[k31_iMon]; i ++) {
-				System.out.print(i + " ");
-				if (i < 10) {
-					System.out.print(" ");
-				}
+				System.out.printf("%4d", i);			
 				k31_iWeekDay ++;
-				if (k31_iWeekDay == 7) {
-					System.out.println();
-					k31_iWeekDay = 0;
-					
+				if (k31_iWeekDay % 7 == 0) {
+					System.out.println();									
 				}				
-			}
-		}
+			} k31_iWeekDay %= 7;
+		} 	
 
 	}
 
